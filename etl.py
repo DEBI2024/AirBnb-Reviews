550eaa848935ac1e6b7221e167b5a91481b9898c
El-Moghazy2 <almoghazy1@gmail.com>
2024-07-25 22:41:06 +0300

    fix: adding new Textblob scores and removing old score

--- etl.py
+++ etl.py
@@ -14,6 +14,8 @@ import pandas as pd
 import scipy.sparse.linalg
 from sklearn.model_selection import train_test_split
 from kaggle.api.kaggle_api_extended import KaggleApi
+from textblob import TextBlob
+
 
 CITIES = ["london", "nyc"]
 COLUMNS = ['id', 'listing_url', 'scrape_id', 'last_scraped', 'name',
@@ -104,38 +106,56 @@ def save_data(data: str, path: str):
     data.to_csv(path)
 
 
+def calculate_scores(data: pd.DataFrame)-> pd.DataFame:
+  """calculate the scores of the comments using textblob
+  Args:
+    data: contains the comments needed to calculate the score
+  Returns:
+    data: same object after adding the needed columns"""
+  
+  subjectivity = []
+  polarity = []
+  for comment in data.comments.astype(str):
+      blob = TextBlob(comment)
+      subjectivity.append(blob.sentiment.subjectivity)
+      polarity.append(blob.sentiment.polarity)
+  
+  data["subjectivity"] = subjectivity
+  data["polarity"] = polarity
+  
+  return data
+
 def split_data(csv_files_names: list):
-    """split the data and save it to the dataset folder to be ready for kaggle upload
-    Args:
-      csv_files_names: the list of all csv files in the folder
-    """
+  """split the data and save it to the dataset folder to be ready for kaggle upload
+  Args:
+    csv_files_names: the list of all csv files in the folder
+  """
 
-    for city in CITIES:
+  for city in CITIES:
 
-        # paths to listings and reviews within for the city
-        city_listings_path = [
-            x for x in csv_files_names if x.startswith("listings") and city in x][0]
-        city_reviews_path = [
-            x for x in csv_files_names if x.startswith("reviews") and city in x][0]
+    # paths to listings and reviews within for the city
+    city_listings_path = [x for x in csv_files_names if x.startswith("listings") and city in x][0]
+    city_reviews_path = [x for x in csv_files_names if x.startswith("reviews") and city in x][0]
 
-        city_listings = pd.read_csv(city_listings_path)[COLUMNS]
-        city_reviews = pd.read_csv(city_reviews_path)
+    city_listings = pd.read_csv(city_listings_path)[COLUMNS]
+    city_reviews = pd.read_csv(city_reviews_path)
 
-        # add city name to help when merginig different cities in the future
-        city_listings["city"] = city
-        data_city = city_listings.rename({"id": "listing_id"}, axis=1).merge(
-            city_reviews, on="listing_id", how="inner")
+    # add city name to help when merginig different cities in the future
+    city_listings["city"] = city
+    data_city = city_listings.rename({"id": "listing_id"}, axis = 1).merge(city_reviews, on = "listing_id", how = "inner")
+    
+    data_city = calculate_scores(data_city)
 
-        # divide the data into 80% training and 20% testing datasets
-        train, test = train_test_split(data_city, test_size=0.2, shuffle=True)
+    # divide the data into 80% training and 20% testing datasets
+    train, test = train_test_split(data_city, test_size=0.2, shuffle = True)
 
-        save_data(train, f"./dataset/{city}_train.csv")
-        save_data(test, f"./dataset/{city}_test.csv")
+    save_data(train, f"./dataset/{city}_train.csv")
+    save_data(test, f"./dataset/{city}_test.csv")
 
-        # free memory
-        del data_city
-        del train
-        del test
+    # free memory
+    del data_city
+    del train
+    del test
 
 
 def main(is_dataset_new):
