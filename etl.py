25117d5a3f3b9bf63f98293477b0bfe447f67a9e
El-Moghazy2 <almoghazy1@gmail.com>
2024-07-28 21:59:47 +0300

    fix: removing \r, fixing beautiful soup part

--- etl.py
+++ etl.py
@@ -20,26 +20,41 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 CITIES = ["london", "nyc"]
-COLUMNS = ['id', 'listing_url', 'scrape_id', 'last_scraped', 'name',
-       'neighborhood_overview', 'host_id', 'number_of_reviews',
-       'number_of_reviews_ltm', 'number_of_reviews_l30d', 'first_review',
-       'last_review', 'review_scores_rating', 'review_scores_accuracy',
-       'review_scores_cleanliness', 'review_scores_checkin',
-       'review_scores_communication', 'review_scores_location',
-       'review_scores_value', 'calculated_host_listings_count','reviews_per_month']
+COLUMNS = [
+    'id',
+    'listing_url',
+    'scrape_id',
+    'last_scraped',
+    'name',
+    'neighborhood_overview',
+    'host_id',
+    'number_of_reviews',
+    'number_of_reviews_ltm',
+    'number_of_reviews_l30d',
+    'first_review',
+    'last_review',
+    'review_scores_rating',
+    'review_scores_accuracy',
+    'review_scores_cleanliness',
+    'review_scores_checkin',
+    'review_scores_communication',
+    'review_scores_location',
+    'review_scores_value',
+    'calculated_host_listings_count',
+    'reviews_per_month']
 
 cities_reviews = {
-    "nyc": "https://data.insideairbnb.com/united-states/" \
-        "ny/new-york-city/2024-07-05/data/reviews.csv.gz",
-    "london": "https://data.insideairbnb.com/united-kingdom/" \
-        "england/london/2024-06-14/data/reviews.csv.gz"
+    "nyc": "https://data.insideairbnb.com/united-states/"
+    "ny/new-york-city/2024-07-05/data/reviews.csv.gz",
+    "london": "https://data.insideairbnb.com/united-kingdom/"
+    "england/london/2024-06-14/data/reviews.csv.gz"
 }
 
 cities_listings = {
-    "nyc": "https://data.insideairbnb.com/united-states/ny/new-york-city/" \
-        "2024-07-05/data/listings.csv.gz",
-    "london": "https://data.insideairbnb.com/united-kingdom/" \
-        "england/london/2024-06-14/data/listings.csv.gz"
+    "nyc": "https://data.insideairbnb.com/united-states/ny/new-york-city/"
+    "2024-07-05/data/listings.csv.gz",
+    "london": "https://data.insideairbnb.com/united-kingdom/"
+    "england/london/2024-06-14/data/listings.csv.gz"
 }
 
 
@@ -105,71 +120,76 @@ def get_file_names():
 
 def save_data(data: str, path: str):
     """save the data to a specific path"""
-    data.to_csv(path, index = False)
+    data.to_csv(path, index=False)
 
 
 def remove_html_tags(data):
-  """remove html tags from comments in the dataframe"""
+    """remove html tags from comments in the dataframe"""
 
-  data["comments"] = data.comments.astype(str)
-  data["comments"].apply(lambda x: BeautifulSoup(x, 'html.parser').get_text())
+    data["comments"] = data.comments.astype(str)
+    data["comments"] = data["comments"].apply(
+        lambda x: BeautifulSoup(x, 'html.parser').get_text())
 
-  return data
+    return data
 
 
 def calculate_scores(data: pd.DataFrame):
-  """calculate the scores of the comments using textblob"""
-  subjectivity = []
-  polarity = []
-  data = remove_html_tags(data)
+    """calculate the scores of the comments using textblob"""
+    subjectivity = []
+    polarity = []
+    data = remove_html_tags(data)
 
-  analyzer = SentimentIntensityAnalyzer()
-  for comment in tqdm(data.comments.astype(str)):
-      vs = analyzer.polarity_scores(comment)
-      polarity.append(vs["compound"])
+    analyzer = SentimentIntensityAnalyzer()
+    for comment in tqdm(data.comments.astype(str)):
+        vs = analyzer.polarity_scores(comment)
+        polarity.append(vs["compound"])
 
-  data["polarity"] = polarity
+    data["polarity"] = polarity
 
-  data.loc[data.polarity <= -0.05, "polarity_class"] = 0
-  data.loc[data.polarity >= 0.05, "polarity_class"] = 1
+    data.loc[data.polarity <= -0.05, "polarity_class"] = 0
+    data.loc[data.polarity >= 0.05, "polarity_class"] = 1
 
-  data = data.dropna(subset = "polarity_class")
-  
-  return data
+    data = data.dropna(subset="polarity_class")
+
+    return data
+
 
 def split_data(csv_files_names: list):
-  """split the data and save it to the dataset folder to be ready for kaggle upload
-  Args:
-    csv_files_names: the list of all csv files in the folder
-  """
+    """split the data and save it to the dataset folder to be ready for kaggle upload
+    Args:
+      csv_files_names: the list of all csv files in the folder
+    """
 
-  for city in CITIES:
+    for city in CITIES:
 
-    # paths to listings and reviews within for the city
-    city_listings_path = [x for x in csv_files_names if x.startswith("listings") and city in x][0]
-    city_reviews_path = [x for x in csv_files_names if x.startswith("reviews") and city in x][0]
+        # paths to listings and reviews within for the city
+        city_listings_path = [
+            x for x in csv_files_names if x.startswith("listings") and city in x][0]
+        city_reviews_path = [
+            x for x in csv_files_names if x.startswith("reviews") and city in x][0]
 
-    city_listings = pd.read_csv(city_listings_path)[COLUMNS]
-    city_reviews = pd.read_csv(city_reviews_path)
+        city_listings = pd.read_csv(city_listings_path)[COLUMNS]
+        city_reviews = pd.read_csv(city_reviews_path)
 
-    # add city name to help when merginig different cities in the future
-    city_listings["city"] = city
-    data_city = city_listings.rename({"id": "listing_id"}, axis = 1).merge(city_reviews, on = "listing_id", how = "inner")
+        # add city name to help when merginig different cities in the future
+        city_listings["city"] = city
+        data_city = city_listings.rename({"id": "listing_id"}, axis=1).merge(
+            city_reviews, on="listing_id", how="inner")
 
-    data_city = calculate_scores(data_city)
-    data_city.comments = data_city.comments.str.replace("\r", " ")
+        data_city = calculate_scores(data_city)
+        data_city.comments = data_city.comments.str.replace("\r", " ")
 
-    # divide the data into 80% training and 20% testing datasets
-    train, test = train_test_split(data_city, test_size=0.2, shuffle = True, stratify = data_city["polarity_class"])
+        # divide the data into 80% training and 20% testing datasets
+        train, test = train_test_split(
+            data_city, test_size=0.2, shuffle=True, stratify=data_city["polarity_class"])
 
+        save_data(train, f"./dataset/{city}_train.csv")
+        save_data(test, f"./dataset/{city}_test.csv")
 
-    save_data(train, f"./dataset/{city}_train.csv")
-    save_data(test, f"./dataset/{city}_test.csv")
-
-    # free memory
-    del data_city
-    del train
-    del test
+        # free memory
+        del data_city
+        del train
+        del test
 
 
 def main(is_dataset_new, upload):
@@ -188,4 +208,5 @@ def main(is_dataset_new, upload):
         if is_dataset_new:
             api.dataset_create_new(folder="./dataset")
         else:
-            api.dataset_create_version(folder="./dataset", version_notes="update")
+            api.dataset_create_version(
+                folder="./dataset", version_notes="update")
